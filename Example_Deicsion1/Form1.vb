728d51f918e15d7ca3ce46591f9c42f56a579609
jirapat kanokmaneeporn <ka.jirapat@gmail.com>
2018-11-20 14:38:36 +0700

    Add Button code

--- Example_Deicsion1/Form1.vb
+++ Example_Deicsion1/Form1.vb
@@ -1,3 +1,25 @@
 ﻿Public Class Form1
 
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim score As Double = CDbl(TextBox1.Text)
+        If score < 0 Or score > 100 Then
+            MessageBox.Show("Score Is Out Of Range ...", "Score", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+        ElseIf score > 79 Then
+            Label1.Text = "A"
+        ElseIf score > 69 Then
+            Label1.Text = "B"
+        ElseIf score > 59 Then
+            Label1.Text = "C"
+        ElseIf score > 49 Then
+            Label1.Text = "D"
+        Else
+            Label1.Text = "F"
+        End If
+
+
+    End Sub
+
 End Class
