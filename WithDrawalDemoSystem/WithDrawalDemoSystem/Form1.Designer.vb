e1bf68237d6f8b9a6f2b196bdf2458f2fa6b01b9
jirapat kanokmaneeporn <ka.jirapat@gmail.com>
2018-11-20 15:34:27 +0700

    Create UI

--- WithDrawalDemoSystem/WithDrawalDemoSystem/Form1.Designer.vb
+++ WithDrawalDemoSystem/WithDrawalDemoSystem/Form1.Designer.vb
@@ -22,10 +22,98 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBalance = New System.Windows.Forms.TextBox()
+        Me.TextWithdraw = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(195, 90)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(66, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "ตกลง"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ยอดเงินคงเหลือ"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "จำนวนที่จะถอน"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 124)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(102, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "ชนิดธนบัตรที่จะถอน"
+        '
+        'TextBalance
+        '
+        Me.TextBalance.Location = New System.Drawing.Point(128, 15)
+        Me.TextBalance.Name = "TextBalance"
+        Me.TextBalance.Size = New System.Drawing.Size(133, 20)
+        Me.TextBalance.TabIndex = 4
+        '
+        'TextWithdraw
+        '
+        Me.TextWithdraw.Location = New System.Drawing.Point(128, 53)
+        Me.TextWithdraw.Name = "TextWithdraw"
+        Me.TextWithdraw.Size = New System.Drawing.Size(133, 20)
+        Me.TextWithdraw.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label4.Location = New System.Drawing.Point(125, 124)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(136, 151)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Label4"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(277, 295)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextWithdraw)
+        Me.Controls.Add(Me.TextBalance)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBalance As TextBox
+    Friend WithEvents TextWithdraw As TextBox
+    Friend WithEvents Label4 As Label
 End Class
